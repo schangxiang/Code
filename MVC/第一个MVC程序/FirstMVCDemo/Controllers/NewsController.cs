c7b1793937a43280daabb0f05b0c9401ee55c86a
schangxiang <schangxiang@126.com>
2019-06-06 09:21:14 +0800

    增加 mvc代码

---
+++ MVC/第一个MVC程序/FirstMVCDemo/Controllers/NewsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCDemo.Controllers
+{
+    public class NewsController : Controller
+    {
+        //
+        // GET: /News/
+        public ActionResult Index()
+        {
+            ViewData["message"] = "你好啊，长祥";
+            ViewBag.message = "你是鬼吗";
+            return View();
+        }
+    }
+}
