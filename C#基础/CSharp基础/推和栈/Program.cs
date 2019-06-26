51b70ab446216d5bbfc6481e63aeecc4278b2a45
schangxiang <schangxiang@126.com>
2019-06-26 10:06:36 +0800

    增加 等值判断

--- C#基础/CSharp基础/推和栈/Program.cs
+++ C#基础/CSharp基础/推和栈/Program.cs
@@ -10,6 +10,9 @@ namespace 堆和栈
     {
         static void Main(string[] args)
         {
+
+            #region 引用类型
+
             // s : 对象的引用【存储在栈中】
             //new Student() 【对象存储在堆中】
             Student s = new Student();//引用类型
@@ -21,6 +24,10 @@ namespace 堆和栈
             Console.WriteLine("引用类型赋值后对象s2的Age是:" + s2.Age.ToString());// 输出：44
             Console.WriteLine("引用类型赋值后对象s的Age是:" + s.Age.ToString());// 输出：88
 
+            #endregion
+
+            #region  值类型
+
             //局部变量 【存储在栈中】
             int aaa = 0;//值类型
             Console.WriteLine("初始aaa变量的值是：" + aaa.ToString()); // 输出：0
@@ -29,6 +36,24 @@ namespace 堆和栈
 
             Console.WriteLine("值类型赋值后aaa变量的值是：" + aaa.ToString()); // 输出：0
             Console.WriteLine("值类型赋值后aaa2变量的值是：" + aaa2.ToString()); // 输出：99
+
+            #endregion
+
+            #region 等值判断
+
+            int i = 3;
+            int j = 3;
+            Console.WriteLine(i == j);// 输出:true ,值类型等值判断直接比较值
+
+            Student a = new Student();
+            a.Age = 3;
+            Student b = new Student();
+            b.Age = 3;
+            Console.WriteLine(a == b);// 输出:false ,引用类型等值判断的是栈中的地址，不是比较数据的本身
+
+            #endregion
+
+
             Console.ReadKey();
 
 
