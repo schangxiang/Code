fbdec14dcfc9c4162495a12ad71e98fd52f913ed
schangxiang <schangxiang@126.com>
2019-07-16 13:29:58 +0800

    增加 实体类

---
+++ C#基础/CSharp基础/Base关键字用法/C.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base关键字用法
+{
+    class C:A
+    {
+        public int age = 99;
+        public C() {
+        }
+        public C(string name) : base(name)
+        {
+            Console.WriteLine(name + "Build C(有参构造)");
+        }
+
+        public override void Hello()
+        {
+            Console.WriteLine("Hello,我是C方法里的Hello方法");
+        }
+    }
+}
