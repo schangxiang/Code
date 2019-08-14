764f1ab69e36387e96c7a90a5ab2100f7c9b3423
schangxiang <schangxiang@126.com>
2019-08-14 08:55:51 +0800

    增加 redis

---
+++ Redis/C#连接Redis/RedisDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisHelper redis = new RedisHelper();
+            
+
+            string key = "csharp:user:1";
+
+            redis.Set(key, "祥哥哥");
+            var value = redis.Get<string>(key);
+            Console.WriteLine("读取的"+key+"值："+value);
+
+            Console.ReadLine();
+        }
+    }
+}
